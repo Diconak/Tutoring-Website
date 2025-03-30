ce633cc95e368dae993a358a08c2a9149745a5ec
Diconi <dimitrijevranic@yahoo.com>
2025-03-30 14:20:40 +0200

    Add add controller logic to router

--- Backend/Routes/Api/lesson.js
+++ Backend/Routes/Api/lesson.js
@@ -1,11 +1,102 @@
-const express = require('express')
-const controller = require('../../Controllers/lesson.js')
-const router = express.Router()
+const express = require("express");
+const lessonSchema = require("../Models/lesson.js");
+const courseSchema = require("../Models/course.js");
+const mongoose = require("mongoose");
 
-router.get('/', controller.getAllLessons)
-router.get('/langName/:langname', controller.getByLangName)
+const lessonModel = lessonSchema.lessonModel;
+const courseModel = courseSchema.courseModel;
+const router = express.Router();
 
-router.post('/addLesson', controller.addLesson)
-router.delete('/deleteLesson/:lessonName', controller.deleteLesson)
+router.get("/", async (req, res, next) => {
+  try {
+    const lessons = await lessonModel
+      .find()
+      .sort({ langName: 1, lessonNumber: 1 })
+      .exec();
+    if (lessons) {
+      res.status(200).json(lessons);
+    } else {
+      res.status(400).json();
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+router.get("/langName/:langname", async (req, res, next) => {
+  const langname = req.params.langName;
+  try {
+    if (!langname) {
+      const error = new Error("Requested langName is missing in url");
+      error.status = 400;
+      throw error;
+    }
+    const lesson = await lessonModel.find({ langName: langname }).exec();
+    if (lesson) {
+      res.status(200).json(lesson);
+    } else {
+      res.status(500).json();
+    }
+  } catch (error) {
+    next(error);
+  }
+});
 
-module.exports = router
+router.post("/addLesson", async (req, res, next) => {
+  const {
+    lessonNumber,
+    langname,
+    lessonname,
+    description,
+    videourl,
+    githuburl,
+  } = req.body;
+
+  try {
+    const newLesson = new lessonModel({
+      _id: mongoose.Types.ObjectId(),
+      lessonNumber: lessonNumber,
+      langName: langname,
+      lessonName: lessonname,
+      description: description,
+      videoURL: videourl,
+      githuburl: githuburl,
+    });
+
+    await newLesson.save();
+    const cours = await courseModel.findOne({ courseName: langname }).exec();
+    let courseLessons = cours.courseLessons;
+    if (cours) {
+      courseLessons.push(newLesson._id);
+      cours.updateOne({
+        courseLessons: courseLessons,
+      });
+      res.status(200).json(newLesson);
+    } else {
+      res.status(404).json();
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+router.delete("/deleteLesson/:lessonName", async (req, res, next) => {
+  const lessonname = req.params.lessonName;
+  if (!lessonname) {
+    const error = new Error("Invalid param has been sent");
+    error.status(500);
+    throw error;
+  }
+  try {
+    const lesson = await lessonModel
+      .deleteOne({ lessonName: lessonname })
+      .exec();
+    if (lesson.deletedCount == 1) {
+      res.status(200).json({ success: true });
+    } else {
+      res.status(400).json();
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = router;
